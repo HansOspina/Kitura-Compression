41a9c6606af16a6563437d0dd2795aeffc584890
Hans Ospina <hansospina@gmail.com>
2017-10-30 16:26:02 -0500

    version of CZlib increased to 0.1.2 to match swift 4 on that repo.

--- Package@swift-4.0.swift
+++ Package@swift-4.0.swift
@@ -30,7 +30,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "1.7.0")),
-        .package(url: "https://github.com/IBM-Swift/CZlib.git", .upToNextMinor(from: "0.1.0"))
+        .package(url: "https://github.com/IBM-Swift/CZlib.git", .upToNextMinor(from: "0.1.2"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
